89fe83cac551db83bec45d5ab0dc3b5aee404a1b
Bohdan Dorosh <bohdan_dorosh@epam.com>
2018-08-29 21:06:11 +0300

    intial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      filename: 'index.html',
+      template: 'index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  // module: {
+  //   rules: [
+  //     { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+  //   ]
+  // }
+};
