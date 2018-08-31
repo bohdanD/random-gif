260734526c5f8b82a85126cea684d1df56db8278
Bohdan Dorosh <bohdan_dorosh@epam.com>
2018-08-31 17:18:00 +0300

    Remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,5 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  // module: {
-  //   rules: [
-  //     { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-  //   ]
-  // }
+  }
 };
